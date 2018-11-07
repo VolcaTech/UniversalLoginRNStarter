4c62440a0297e5cf15965588e795ebc2672ebc27
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-11-07 16:29:34 +0300

    connecting views added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
-import ethers, {Wallet} from 'ethers';
-import EthereumIdentitySDK from 'universal-login-monorepo/universal-login-sdk';
 import IntroScreen from './IntroScreen';
 
 Navigation.registerComponent('IntroScreen', () => IntroScreen);
 
-
-// export default class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-// 	this._testEthers();
-// 	this.startApp();
-//     }
-
-//     async _testEthers() {
-// 	const privateKey = await ethers.Wallet.createRandom().privateKey;
-// 	console.log({privateKey});
-// 	const jsonRpcUrl = "http://localhost:18545";
-// 	const relayerUrl = "http://localhost:3311";
-// 	const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl);	
-// 	const sdk = new EthereumIdentitySDK(relayerUrl, provider);
 	
-// 	sdk.start();
-// 	console.log("SDK INITED");
-
-// 	const result = await sdk.create(
-// 	    'alexxxx2.mylogin.eth'
-// 	);
-// 	console.log("identityAddress: " + result);	
-//     }
-
-
-//    startApp(root, params = null) {
 Navigation.startSingleScreenApp({
     screen: {
         screen: 'IntroScreen', // unique ID registered with Navigation.registerScreen
+	title: 'Universal Login Starter',
         navigatorStyle: {
             orientation: 'portrait',
             //screenBackgroundColor: '#242836',
@@ -44,6 +17,4 @@ Navigation.startSingleScreenApp({
         navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
     },
 });
-//     }
-// }
 
--- src/IntroScreen.jsx
+++ src/IntroScreen.jsx
@@ -1,13 +1,141 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, Button, Text, StyleSheet, TextInput } from 'react-native';
+import ethers, {Wallet} from 'ethers';
+import EthereumIdentitySDK from 'universal-login-monorepo/universal-login-sdk';
+
+
+class IntroScreen extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    view: 'login',
+	    username: '',
+	    error: null
+	};
+	this.provider = new ethers.providers.JsonRpcProvider('http://localhost:18545');
+	this.sdk = new EthereumIdentitySDK('http://localhost:3311', this.provider);
+    }
+
+    async componentDidMount() {
+	await this.sdk.start();
+    }
+
+    componentWillUnmount() {
+	this.subscription.remove();
+	this.sdk.stop();
+    }
+
+    _getLabelStub() {
+	return {
+	    ipAddress: '192...',
+	    name: 'React Native',
+	    city: '',
+	    time: '',
+	    os: 'React Native',
+	    version: ''
+	};
+    }    
+    
+    async _connect(username) {
+	const jsonRpcUrl = "http://localhost:18545";
+	const relayerUrl = "http://localhost:3311";
+	const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl);	
+	const sdk = new EthereumIdentitySDK(relayerUrl, provider);
+	
+	const name = `${username}.mylogin.eth`;
+	const identityAddress = await this.sdk.identityExist(name);
+	this.identityAddress = identityAddress;
+	if (identityAddress) {
+	    const privateKey = await this.sdk.connect(identityAddress, this._getLabelStub());
+	    this.privateKey = privateKey;
+	    const { address } = new Wallet(privateKey);
+	    // this.subscription = this.sdk.subscribe('KeyAdded', identityAddress, (event) => {
+	    // 	if (event.address === address) {
+	    // 	    this.setState({view: 'connected'});
+	    // 	}
+	    // });
+	} else {
+	    throw new Error(`Identity ${name} does not exist.`);
+	}
+    }
+
+    async _onSubmit() {
+	this.setState({error: null});	
+	try { 
+	    await this._connect(this.state.username);
+	} catch (err) {
+	    console.log({err})
+	    this.setState({error: err.message});
+	}
+    }
+
+    _renderLoginForm() {
+	return (
+	    <View>
+                <View style={styles.row}>
+                    <Text style={styles.title}>Type an username</Text>
+                </View>
+                <View style={styles.row}>
+		  <View style={{flexDirection: 'row'}}>
+		    <View style={{flex: 1}}>
+		      <TextInput
+			 style={styles.textInput}
+			 onChangeText={(username) => this.setState({username})}
+			placeholder="e.g. alice"
+			value={this.state.username}
+			/>
+		    </View>
+		    <View style={{flex: 1}}>
+		      <Text style={{fontSize: 20, marginTop:10}}>.mylogin.eth</Text>
+		    </View>
+		  </View>
+		</View>
+                <View style={styles.row}>
+		  <Button title="Connect" onPress={this._onSubmit.bind(this)}/>
+                </View>
+                <View style={styles.row}>
+		  <Text style={styles.error}>{this.state.error}</Text>
+                </View>		
+	    </View>
+	);
+    }
+    
+    _renderConnectedView() {
+	return (
+	    <Text>Connected!</Text>
+	);
+    }
+
+    _renderContent() {
+	if (this.state.view === 'login') {
+	    return this._renderLoginForm();
+	} else if (this.state.view === 'connected') {
+	    this._renderConnectedView();
+	}
+    }
+    
+    render() {
+        return (
+            <View style={styles.screenContainer}>
+	      {	this._renderContent() }
+            </View>
+        );
+    }
+}
+
 
 const styles = StyleSheet.create({
-    screenContainer: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        backgroundColor: '#fff'
+    textInput: {
+	height: 40,
+	borderColor: 'gray',
+	borderWidth: 1
     },
+    // screenContainer: {
+    //     flex: 1,
+    //     // flexDirection: 'column',
+    //     // justifyContent: 'space-between',
+    //     backgroundColor: '#fff'
+    // },
     title: {
         paddingTop: 30,
         color: 'blue',
@@ -15,44 +143,13 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold'
     },
-    buttonContainer: {
-        marginTop: 20,
-        width: 200,
-        height: 50,
-        borderWidth: 2
+    row: {
+ 	padding: 10
     },
-    buttonText: {
-        textAlign: 'center',
-        fontSize: 20,
-        paddingTop: 10,
-        color: 'black'
-    },
-    centeredFlex: {
-        flex: 1,
-        alignItems: 'center'
+    error: {
+	color: 'red'
     }
 });
-
-
-class IntroScreen extends React.Component {
-
-
-    
-    render() {
-        return (
-            <View style={styles.screenContainer}>
-                <View style={styles.centeredFlex}>
-                    <Text style={styles.title}>Type an username</Text>
-                </View>
-                <View style={styles.centeredFlex}>
-                    <TouchableOpacity style={styles.buttonContainer} onPress={() => this.props.navigator.push({ screen: 'dailywallet.PasscodeSetScreen1' })}>
-                        <Text style={styles.buttonText}>Connect</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    }
-}
 
 
 export default IntroScreen;
